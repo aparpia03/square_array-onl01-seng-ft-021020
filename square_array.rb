c9c7b7ae07d6730aeb16412365600239b575a552
Abbas Parpia <aparpia03@gmail.com>
2020-02-13 21:49:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,13 @@
- def square_array(array)
-  # your code here
-  #create a new empty array
-  numbers = [] 
-  #take array and use #each on it, then make | | blocks with new variable 
-  #put each new element in numbers array with the push method and square each element of the array before it goes into the number array.
-  array.each do |square| numbers.push(square*square)
+def square_array(numbers)
+   # create an empty array
+  squared_array = []
+  
+  #call the .each method on the numbers array/parameter 
+numbers.each do |number|  
+
+   #square each element in the numbers array and then shovel each item to squared_array
+   squared_array << number*number 
   end
-  # return numbers and that prints out the new array with each number squared
-  return numbers 
+  #return squared_array
+  squared_array 
 end
